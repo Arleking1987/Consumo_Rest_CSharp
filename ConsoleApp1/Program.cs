68d9d17b339a6d1b4f584da4b47503d6295f6e77
Yonhatan Ramirez <yonhatan.ramirez@softwareone.com>
2022-09-14 21:18:32 -0500

    Se consume un api por método post

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var url = "url del api";
+            var client = new HttpClient();
+            NumeroOrden orden = new NumeroOrden()
+            {
+                numeroOrden = "123456"
+            };
+
+            var data = JsonSerializer.Serialize<NumeroOrden>(orden);
+            HttpContent content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+            var httpResponse = await client.PostAsync(url, content);
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var result = await httpResponse.Content.ReadAsStringAsync();
+                Console.WriteLine(result);
+                var postResult = JsonSerializer.Deserialize<NumeroOrden>(result);
+            }
+            
+        }
+
+
+        public class NumeroOrden
+        {
+            public string numeroOrden { get; set; }
+        }
+    }
+}
